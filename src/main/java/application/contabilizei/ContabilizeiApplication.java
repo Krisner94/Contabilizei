286ecd82f2fda30f3aeffa0aee11b46d4f211f29
Rhama Krisner <rhamakrisner@gmail.com>
2024-03-20 01:20:21 -0300

    Criado projeto OCntabilizei

---
+++ src/main/java/application/contabilizei/ContabilizeiApplication.java
@@ -0,0 +1,13 @@
+package application.contabilizei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilizeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContabilizeiApplication.class, args);
+    }
+
+}
