bcaee26e77bef4932a1a9559fb8ad084b2aaf0b1
Rhama Krisner <rhamakrisner@gmail.com>
2024-03-20 01:20:21 -0300

    Criado projeto Contabilizei

---
+++ src/test/java/application/contabilizei/ContabilizeiApplicationTests.java
@@ -0,0 +1,13 @@
+package application.contabilizei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabilizeiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
